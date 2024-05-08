4c4738002483777afdf131444a4d52ddf53273c9
viv151 <vivekhjat@gmail.com>
2024-05-08 18:26:20 +0530

    rm-total

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -19,7 +19,7 @@ const Homepage = () => {
     <>
       <Title level={2} className='heading'>Global Crypto Stats</Title>
       <Row>
-          <Col span={12}><Statistic title= 'Total Cryptocurrencies' value={globalStats.total} /></Col>
+          {/* <Col span={12}><Statistic title= 'Total Cryptocurrencies' value={globalStats.total} /></Col> */}
           <Col span={12}><Statistic title= 'Total Exchanges' value={millify(globalStats.totalExchanges)} /></Col>
           <Col span={12}><Statistic title= 'Total Market Cap' value={millify(globalStats.totalMarketCap)} /></Col>
           <Col span={12}><Statistic title= 'Total 24h Volume' value={millify(globalStats.total24hVolume)} /></Col>
